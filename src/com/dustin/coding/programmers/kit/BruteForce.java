a53d3cc64eebf187783766921a747709190b222f
Pyeon Huijin <zzangs33@hanmail.net>
2021-06-13 16:27:01 +0900

    Upload bruteforce

--- src/com/dustin/coding/programmers/kit/BruteForce.java
+++ src/com/dustin/coding/programmers/kit/BruteForce.java
@@ -1,19 +1,20 @@
 package com.dustin.coding.programmers.kit;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class BruteForce {
 
+    Set<Integer> totalNumbers = new HashSet<>();
+
     public int[] virtualTest(int[] answers) {
         Map<Integer, Integer> correct = new HashMap<>();
-        int[][] myWay = new int[][]{{5, 1, 2, 3, 4}, {5, 2, 1, 2, 3, 2, 4, 2}, {5, 3, 3, 1, 1, 2, 2, 4, 4, 5}};
+        var myWay = new int[][]{{5, 1, 2, 3, 4}, {5, 2, 1, 2, 3, 2, 4, 2}, {5, 3, 3, 1, 1, 2, 2, 4, 4, 5}};
         correct.put(1, 0);
         correct.put(2, 0);
         correct.put(3, 0);
 
-        for (int i = 1; i <= answers.length; i++) {
+        for (var i = 1; i <= answers.length; i++) {
             int answer = answers[i - 1];
             if (myWay[0][i % 5] == answer) {
                 Integer val = correct.get(1);
@@ -29,12 +30,12 @@ public class BruteForce {
             }
         }
 
-        int[] rank = correct.entrySet().stream()
+        var rank = correct.entrySet().stream()
                 .sorted((e1, e2) -> {
                     if (e1.getValue() < e2.getValue()) return 1;
                     else if (e1.getValue() > e2.getValue()) return -1;
 
-                    return e1.getKey() < e2.getKey() ? -1 : 1;
+                    return Integer.compare(e1.getKey(), e2.getKey());
                 })
                 .map(Map.Entry::getKey).mapToInt(Integer::intValue).toArray();
 
@@ -44,8 +45,26 @@ public class BruteForce {
     }
 
     public int findPrimeNumber(String numbers) {
+        var split = numbers.split("");
+        var visited = new boolean[split.length];
+        for (var i = 1; i <= split.length; i++)
+            addNumbers(split, visited, "", i);
 
-        return 0;
+        return totalNumbers.size();
+    }
+
+    private void addNumbers(String[] split, boolean[] visited, String current, int targetSize) {
+        if (current.length() == targetSize) {
+            var res = Integer.parseInt(current);
+            if (isPrime(res)) totalNumbers.add(res);
+        }
+        for (var i = 0; i < split.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                addNumbers(split, visited, current + split[i], targetSize);
+                visited[i] = false;
+            }
+        }
     }
 
     private boolean isPrime(int number) {
@@ -55,6 +74,16 @@ public class BruteForce {
     }
 
     public int[] carpet(int brown, int yellow) {
+        int result = brown + yellow;
+        List<Integer> verticals = new ArrayList<>();
+        for (var i = 1; i <= result / i; i++) {
+            if (result % i == 0) verticals.add(i);
+        }
+        for (int vert : verticals) {
+            int edge = 2 * vert + 2 * (result / vert) - 4;
+            if (edge == brown) return new int[]{result / vert, vert};
+        }
+
         return new int[0];
     }
 }
